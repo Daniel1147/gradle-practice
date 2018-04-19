7269b7a5a42af25bf405613e1d28c6b37f576daf
daniel1147 <sxn91401@gmail.com>
2018-04-19 15:45:07 +0000

    import org.apache.commons.lang3.StringUtils

--- src/main/com/daniel1147/gradleTutorial/Main.java
+++ src/main/com/daniel1147/gradleTutorial/Main.java
@@ -1,9 +1,10 @@
 package com.daniel1147.gradleTutorial;
 
 import java.util.Arrays;
+import org.apache.commons.lang3.StringUtils;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(args));
+    System.out.println(StringUtils.swapCase(Arrays.toString(args)));
   }
 }
