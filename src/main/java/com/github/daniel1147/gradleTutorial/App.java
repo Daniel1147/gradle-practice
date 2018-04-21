c081d93ca82f950a13fe136dda7e6374aaf1612f
daniel1147 <sxn91401@gmail.com>
2018-04-21 14:10:22 +0000

    public App for document generation, add comments to this class

--- src/main/java/com/github/daniel1147/gradleTutorial/App.java
+++ src/main/java/com/github/daniel1147/gradleTutorial/App.java
@@ -1,10 +1,16 @@
 package com.github.daniel1147.gradleTutorial;
 
-class App {
+/** A class for demo and test purpose.
+ */
+public class App {
+  /** Get a zero!
+   */
   public int zero() {
     return 0;
   }
 
+  /** Get class name!
+   */
   public String className() {
     return "App";
   }
